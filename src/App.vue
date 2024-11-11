ea357c4f709d7ba37b8cb12cc1c6c01d5522bfbd
N1VE <enderboygaming46@gmail.com>
2024-11-11 21:41:28 +0700

    ok chua

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import HelloWorld from './components/homepage.vue';
+import BlogListing from './components/bloglisting.vue';
 </script>
 
 <template>
   <HelloWorld></HelloWorld>
+  <BlogListing></BlogListing>
 </template>
