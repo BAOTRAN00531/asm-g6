d9d0770129cf7c7e81e05c4b041a0050d4731517
Andrew0456 <115640972+Andrew0456@users.noreply.github.com>
2024-11-12 15:13:23 +0700

    Update main.js
    .

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import { createApp } from 'vue'
 import './assets/main.css';
 import App from './App.vue'
 
+// Import các thành phần cần thiết từ Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faEnvelope, faPhone, faGlobe, faMapMarkerAlt, faCog } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Thêm các icon vào thư viện
+library.add(faUser, faEnvelope, faPhone, faGlobe, faMapMarkerAlt, faCog)
+
+const app = createApp(App)
+
+// Đăng ký component FontAwesomeIcon toàn cục
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+
 createApp(App).mount('#app')
